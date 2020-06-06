47bbe13c300589ff76e466e568b71afb53affd3b
SB-74 <scottbibby01@gmail.com>
2020-06-06 17:21:22 +1200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+$(function(){
+    $("button").on("click", function(){
+        var url = "https://github.com/SB-74/Start/blob/99f44364831b2f6d1402e37541bc6039a55dda56/Textfile12.txt";
+        $("div.main").load(url);
+        alert("CLICK");
 
+    });
+});
